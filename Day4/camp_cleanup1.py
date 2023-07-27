f19254ace2f1288b7f8a6b6f80ad5c14157ab47c
tianzhi-george-li <tianzhi-george-li@users.noreply.github.com>
2023-07-26 20:54:12 -0400

    day4

---
+++ Day4/camp_cleanup1.py
@@ -0,0 +1,23 @@
+def overlap(pair):
+    elves = (pair.strip()).split(",")
+    elf1 = list(map(int, elves[0].split("-")))
+    elf2 = list(map(int, elves[1].split("-")))
+
+    if elf1[0] <= elf2[0] and elf1[1] >= elf2[1]:
+        return 1
+    elif elf2[0] <= elf1[0] and elf2[1] >= elf1[1]:
+        return 1
+    else:
+        return 0
+
+def main():
+    with open("Day4\input.txt") as f:
+        input = f.readlines()
+
+    count = 0
+    for pair in input:
+        count += overlap(pair)
+    print(count)
+
+if __name__ == "__main__":
+    main()
